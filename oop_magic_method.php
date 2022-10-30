fc36f8d10022d76cb5e2f698822b019337f99741
Md.Ashraful Momen <34467735+Ashraful-Momen@users.noreply.github.com>
2022-10-30 23:08:33 +0600

    __Magic Methods__
    isset,unset,call,callstatic,sleep,wakeup..... HW: invoke and clone

--- oop_magic_method.php
+++ oop_magic_method.php
@@ -1,4 +1,139 @@
 <?php
+<?php
+#=============================__get(), __call()=========================
+// class A {
+//     private $name="Ashraful Momen";
+//     public function display()
+//     {
+//         echo " I'm from class A ";
+//     }
+
+//     function __get($property){
+//         echo "\nError popup for Variable or property : Not exit the propertiy/vaiable ";
+//     }
+//     function __call($name, $arguments) # Error : use for undefine method 
+//     {
+//         echo "\n Error popup for Methods : Not exit the propertiy {$name} or private methods";
+//     }
+// }
+
+// $obj = new A();
+
+// $obj->age();
+// $obj->age;
+#====================================================================
+// class A {
+//  	private $Name1;
+// 	private $Name2;
+
+// 	private function setName($fname, $lname){
+// 		$this->Name1 = $fname;
+// 		$this->Name2 = $lname;
+// 	}
+//     function display()
+//     {
+//         echo "First Name: $this->Name1";
+//         echo "\n Last Name: $this->Name2\n";
+//     }
+//     function __get($property)
+//     {
+//         echo "\n$property = is not found or private property";
+//     }
+//     function __call($method, $argc){
+//         if(method_exists($this,$method)){
+//             call_user_func_array([$this,$method],$argc);
+//         }
+//         else{
+//             echo "\n $method = does not exit ";
+//         }
+//     }
+
+// }
+// $obj=new A();
+// $obj->setName("Ashraful","Momen");
+
+// $obj->display();
+// $obj->age();
+// $obj->name;
+
+
+// echo "<pre>";
+// print_r($obj);
+// echo "</pre>";
+#======================================__call static method==============================================
+
+// class A {
+//     private $Name;
+//     private static function show($name){
+//         echo " Static Name=$name\n";
+//     }
+//     public function display($name){
+//         echo "nonstatic Name=$name\n";
+//     }
+//     public static function __callStatic($method, $argc)
+//     {
+//         if(method_exists(__CLASS__,$method)){
+//             call_user_func_array([__CLASS__,$method],$argc);
+//         }
+//         else{
+//             echo "the method not exit : $method and value =$argc\n";
+//         }
+//     }
+
+
+// }
+
+
+
+// A::show("Md Ashraful momen");
+
+// A::name("mukti");
+
+// $obj=new A();
+// $obj->display("Shuvo");
+
+#======================___isset() :==========================
+
+// class A {
+//     public $name="Momen";
+   
+//     public function __isset($name) 
+//     {
+//         if(property_exists($this,$name))
+//         {return true;}
+//         return "Nai";
+//     }
+    
+// }
+
+// $obj = new A ();
+// var_dump(isset($obj->name));
+
+
+// echo isset($obj->name);
+#============================================================
+// class User
+// {
+//   private $data = [
+//     'name' => 'John',
+//     'age' => 34,
+//   ];
+
+//   public function __isset($name)
+//   {
+//     if (array_key_exists($name, $this->data)) {
+//       return true;
+//     }
+
+//     return false;
+//   }
+// }
+
+// $user = new User();
+
+// var_dump(isset($user->name));
+
+#===================End is isset()==============================
 #---------------------------__unset(): Distroy /Delete the property with value -----------------
 //  class A{
 //     public $course="php";
