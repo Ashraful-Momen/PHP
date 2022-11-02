3c34cf722d525220e74856a4a9d76ea0b1aa3a18
Md.Ashraful Momen <34467735+Ashraful-Momen@users.noreply.github.com>
2022-11-02 14:03:46 +0600

    PHP Array Full
    PHP Array Full

---
+++ PHP Array/array2.php
@@ -0,0 +1,60 @@
+<?php 
+#Associative Array:______________________________________
+
+
+// $color = array ('red'=>13, 20=>"green");
+#or
+// $color = ['red'=>13, 20=>"green"];
+
+// echo $color['red']."<br>";
+// echo $color[20];
+
+#===========================================
+
+
+//   $colors = [
+//     "red",
+//     "green",
+//     "blue"
+//   ];
+
+// foreach($colors as $value){
+// echo $value."<br>";
+// }
+
+// $alphabet= [
+//     'a','b','c'
+// ];
+
+// foreach ($alphabet as $v){
+//     echo $v."<br>";
+// }
+
+#============================
+
+// $age = [
+//     "bill" => 25,
+//     "steve" => 28,
+//     "elon" => 22,
+//   ];
+
+
+//   $key = array_keys($age);
+//   $value= array_values($age);
+
+// for($i=0;$i<3;$i++){
+//     echo $key[$i]."=".$value[$i]."<br>";
+    
+// }
+
+
+// foreach($age as $k => $v){
+// echo "$k = $v <br>";
+// }
+
+
+// echo "<ul>";
+// foreach($age as $key => $value){
+//   echo "<li>$key = $value </li>";
+// }
+// echo "</ul>";
