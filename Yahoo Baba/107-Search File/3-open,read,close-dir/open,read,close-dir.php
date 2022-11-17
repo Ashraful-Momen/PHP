1175ff2ddeda2f845e7844c7a7bf9bff2f1edc2f
Md.Ashraful Momen <34467735+Ashraful-Momen@users.noreply.github.com>
2022-11-18 00:17:08 +0600

    Open, Read, Close -Dir
    Open, Read, Close -Dir

---
+++ Yahoo Baba/107-Search File/3-open,read,close-dir/open,read,close-dir.php
@@ -0,0 +1,43 @@
+<?php 
+
+
+#opendir(),readdir(),closedir().
+
+
+// $dir = ".";
+
+// if(is_dir($dir)){
+//     if($d = opendir($dir)){
+//         while($file = readdir($d)){
+//             echo $file."<br>";
+//         }
+//     }
+// }
+
+// closedir();
+
+/* $dir = "..";
+
+if (is_dir($dir)){
+  if ($d = opendir($dir)){
+    while (($file = readdir($d))){
+      echo "filename:" . $file . "<br>";
+    }
+    closedir($d);
+  }
+}
+ */
+
+
+/* $dir = "../";
+
+if (is_dir($dir)){
+  if ($d = opendir($dir)){
+    while (($file = readdir($d))){
+      echo "filename:" . $file . "<br>";
+    }
+    closedir($d);
+  }
+} */
+?>
+?>
