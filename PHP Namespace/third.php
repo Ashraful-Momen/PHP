ac99dd07a12695c680865d8352d4ec8496974b2a
Md.Ashraful Momen <34467735+Ashraful-Momen@users.noreply.github.com>
2024-06-24 17:09:46 +0600

    How to use the namespace
    namespace is used for call class, function one file to another file.

--- PHP Namespace/third.php
+++ PHP Namespace/third.php
@@ -2,6 +2,10 @@
 require "first.php";
 require "second.php";
 
+//Namespace help to call class,function ... etc one php file to another php file . 
+//in first.php file has namespace first; and another function name is test(). 
+//now if we want to call first.php classe->function into the third.php file , then we just create object. 
+
 $obj=new first\test();
 $obj->show();
 
